1b040a021a596731f18f7a4aa2bb080273c7bbda
Zin_User <as7227020@gmail.com>
2024-05-14 03:00:27 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,31 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-            {//增加Line頭像 這樣才可以顯示 詳細可以去DB看image欄位的url 只要前半段即可 
+            {//增加gitHub頭像 這樣才可以顯示
               protocol: "https",
-              hostname: "profile.line-scdn.net",
+              hostname: "avatars.githubusercontent.com",
             },
-        ],
+            {//增加google頭像 這樣才可以顯示 詳細可以去DB看image欄位的url 只要前半段即可 
+              protocol: "https",
+              hostname: "lh3.googleusercontent.com",
+            },
+              {//增加Line頭像 這樣才可以顯示 詳細可以去DB看image欄位的url 只要前半段即可 
+                protocol: "https",
+                hostname: "profile.line-scdn.net",
+              },
+            {
+              protocol: "https",
+              hostname: "book-thumbnail-bucket.s3.ap-northeast-1.amazonaws.com",
+            },
+            {
+              protocol: "https",
+              hostname: "images.microcms-assets.io",
+            },
+            {
+              protocol: "https",
+              hostname: "images.microcms-assets.io",
+            },
+          ],
       },
     reactStrictMode: false,
 };
