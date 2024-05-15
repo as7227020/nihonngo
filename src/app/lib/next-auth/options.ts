f3317e30f62eedaf5438eecf5882b53eadba6eb8
Zin_User <as7227020@gmail.com>
2024-05-15 20:03:00 +0800

    line email test

--- src/app/lib/next-auth/options.ts
+++ src/app/lib/next-auth/options.ts
@@ -10,6 +10,7 @@ export const nextAuthOptions: NextAuthOptions = {
     LineProvider({
       clientId: process.env.LINE_CLIENT_ID!,
       clientSecret: process.env.LINE_CLIENT_SECRET!,
+
       //issuer:'https://nihonngo.vercel.app/auth/realms/Test'
     }),
     GithubProvider({
@@ -25,6 +26,7 @@ export const nextAuthOptions: NextAuthOptions = {
         user: {
           ...session.user,
           id: user.id,
+          email: user.email,
         },
       };
     },
