019885a2cb8feb7ba6ee46325cdcbc94eae69118
Zin_User <as7227020@gmail.com>
2024-05-15 21:15:29 +0800

    首頁顯示優化

--- src/app/components/UI/navbar/navbarUI.tsx
+++ src/app/components/UI/navbar/navbarUI.tsx
@@ -101,47 +101,18 @@ const NavbarUI = () => {
                 單字打拼
               </a>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+
             <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="#"
-                tabIndex={-1}
-                aria-disabled="true"
+              <button
+                onClick={() => {
+                  signOut();
+                }}
+                style={{
+                  border: "none",
+                }}
               >
-                Disabled
-              </a>
+                登出
+              </button>
             </li>
           </ul>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import LoginView from "./page/loginView/page";
 import toast from "react-hot-toast";
+import { GetUserData } from "./components/user/getUserData";
+import { User } from "./types/type";
+import LoadingView from "./components/UI/loadingView";
 
 export default function Home() {
+  const [loadingViewController, SetloadingViewController] = useState(true);
   const test = async () => {
     console.log("click");
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
@@ -32,93 +36,123 @@ export default function Home() {
     });
   };
 
+  const [userData, SetuserData] = useState<User>();
+  const CheckIsLongin = async () => {
+    GetUserData().then((res) => {
+      console.log("login?");
+      console.log(res);
+      if (res == null) {
+        SetuserData(undefined);
+      } else {
+        SetuserData({
+          id: res?.id!,
+          name: res?.name!,
+          email: res?.email!,
+          emailVerified: res?.emailVerified!,
+          image: res?.image!,
+        });
+      }
+    });
+  };
+
   useEffect(() => {
+    SetloadingViewController(true);
+    CheckIsLongin();
     GetProviders();
+    SetloadingViewController(false);
   }, []);
 
   return (
     <div className="container p-2">
+      <LoadingView viewSwitch={loadingViewController} />
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">日文學習網站へようこそ！</h5>
           <p className="card-text">ここでは漢字をたぶっり練習できます。</p>
-          <p className="card-text">
-            以下で登録すれば、練習の履歴が記録されます。
-          </p>
-          <p className="card-text">
-            登録してない場合は、Webサイト利用できますが、履歴の記録はありません。
-          </p>
-          <div className="form-check mt-4">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-              checked={emailOk}
-              onChange={(e) => {
-                SetemailOk(e.target.checked);
-              }}
-            />
-            <label className="form-check-label" htmlFor="flexCheckDefault">
-              登録する際、メールは当サイトに記録されるため、ご了承ください。
-            </label>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <button
-              onClick={() => {
-                if (emailOk == false) {
-                  toast.error("メールの同意チェック必要です。");
-                  return;
-                }
-                signIn("line", { callbackUrl: "/page/loginView" });
-              }}
-              style={{
-                fontSize: "1.2rem",
-                borderRadius: "10px",
-                border: "1px solid #ffffff1",
-                width: "200px",
-                height: "50px",
-                background: "#68e45c",
-                color: "#ffffff",
-                fontWeight: "500",
-                boxShadow: "0px 1px 5px #838282",
-                margin: "5px",
-              }}
-              id="loginLine"
-            >
-              LINE 登録
-            </button>
-            <button
-              onClick={() => {
-                if (emailOk == false) {
-                  toast.error("メールの同意チェック必要です。");
-                  return;
-                }
-                signIn("google", { callbackUrl: "/page/loginView" });
-              }}
-              style={{
-                fontSize: "1.2rem",
-                borderRadius: "10px",
-                border: "1px solid #ffffff1",
-                width: "200px",
-                height: "50px",
-                background: "#3b77c7",
-                color: "#ffffff",
-                fontWeight: "500",
-                boxShadow: "0px 1px 5px #838282",
-                margin: "5px",
-              }}
-              id="loginGoogle"
-            >
-              Google 登録
-            </button>
-          </div>
+
+          {userData != null && userData?.name != "" ? (
+            <div>HELLO {userData.name}</div>
+          ) : (
+            <div>
+              <p className="card-text">
+                以下で登録すれば、練習の履歴が記録されます。
+              </p>
+              <p className="card-text">
+                登録してない場合は、Webサイト利用できますが、履歴の記録はありません。
+              </p>
+              <div className="form-check mt-4">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                  checked={emailOk}
+                  onChange={(e) => {
+                    SetemailOk(e.target.checked);
+                  }}
+                />
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                  登録する際、メールは当サイトに記録されるため、ご了承ください。
+                </label>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <button
+                  onClick={() => {
+                    if (emailOk == false) {
+                      toast.error("メールの同意チェック必要です。");
+                      return;
+                    }
+                    signIn("line", { callbackUrl: "/page/loginView" });
+                  }}
+                  style={{
+                    fontSize: "1.2rem",
+                    borderRadius: "10px",
+                    border: "1px solid #5da16e",
+                    width: "200px",
+                    height: "50px",
+                    background: "#68e45c",
+                    color: "#ffffff",
+                    fontWeight: "500",
+                    boxShadow: "0px 1px 5px #838282",
+                    margin: "5px",
+                  }}
+                  id="loginLine"
+                >
+                  LINE 登録
+                </button>
+                <button
+                  onClick={() => {
+                    if (emailOk == false) {
+                      toast.error("メールの同意チェック必要です。");
+                      return;
+                    }
+                    signIn("google", { callbackUrl: "/page/loginView" });
+                  }}
+                  style={{
+                    fontSize: "1.2rem",
+                    borderRadius: "10px",
+                    border: "1px solid #3d4a81",
+                    width: "200px",
+                    height: "50px",
+                    background: "#3b77c7",
+                    color: "#ffffff",
+                    fontWeight: "500",
+                    boxShadow: "0px 1px 5px #838282",
+                    margin: "5px",
+                  }}
+                  id="loginGoogle"
+                >
+                  Google 登録
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
