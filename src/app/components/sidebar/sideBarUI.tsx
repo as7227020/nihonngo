ce1cb23a53041be02972c63c2f86dd10c1799079
Zin_User <as7227020@gmail.com>
2024-05-17 22:46:40 +0800

    登出後頁面設定

--- src/app/components/sidebar/sideBarUI.tsx
+++ src/app/components/sidebar/sideBarUI.tsx
@@ -190,7 +190,7 @@ export default function SideBarUI({ child }: SideBarUIProps) {
               <a
                 href="#"
                 className="text-decoration-none px-3 py-2 d-block"
-                onClick={() => signOut()}
+                onClick={() => signOut({ callbackUrl: "/" })}
               >
                 <i className="fal fa-bars"></i>
                 登出
